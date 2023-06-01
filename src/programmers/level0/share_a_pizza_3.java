4b4c7cfc1797ae7fb34966d1dcd525e30219f522
leekun <dlrms6172@naver.com>
2023-06-01 22:08:40 +0900

    프로그래머스 > level 0.피자 나눠 먹기(3)

---
+++ src/programmers/level0/share_a_pizza_3.java
@@ -0,0 +1,29 @@
+package programmers.level0;
+
+/**
+ * TITLE : 피자 나눠 먹기(3)
+ * LEVEL : 0
+ */
+public class share_a_pizza_3 {
+
+    public static int solution(int slice, int n) {
+        int answer = 0;
+
+        answer = n % slice;
+
+        if (n % slice == 0) {
+            answer = n / slice;
+        }else{
+            answer = n / slice + 1;
+        }
+
+        System.out.println(answer);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        solution(7, 10);
+        solution(4, 12);
+    }
+}

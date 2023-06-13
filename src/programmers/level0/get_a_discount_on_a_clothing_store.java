ead5d35b2873722c97b9e5801f048c87c2c344e8
leekun <dlrms6172@naver.com>
2023-06-13 23:05:54 +0900

    로그래머스 > level 0.옷가게 할인 받기

---
+++ src/programmers/level0/get_a_discount_on_a_clothing_store.java
@@ -0,0 +1,28 @@
+package programmers.level0;
+
+public class get_a_discount_on_a_clothing_store {
+
+    public static int solution(int price) {
+        int answer = 0;
+
+        if(price >= 500000) {
+            answer = (int)(price * 0.8);
+        } else if(price >= 300000) {
+            answer = (int)(price * 0.9);
+        } else if(price >= 100000) {
+            answer = (int)(price * 0.95);
+        }else {
+            answer = price;
+        }
+
+
+        System.out.println(answer);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        solution(150000);
+        solution(580000);
+        solution(90010);
+    }
+}

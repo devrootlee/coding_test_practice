1a13e5084b2ddbaece354add20fcc99e52186a8e
leekun <dlrms6172@naver.com>
2023-06-15 23:59:27 +0900

    로그래머스 > level 0.문자열안에 문자열

---
+++ src/programmers/level0/string_in_string.java
@@ -0,0 +1,28 @@
+package programmers.level0;
+/**
+ * TITLE : 문자열안에 문자열
+ * LEVEL : 0
+ */
+public class string_in_string {
+
+    public static int solution(String str1, String str2){
+        int answer = 0;
+
+        if(str1.contains(str2)){
+            answer = 1;
+        }
+        else{
+            answer = 2;
+        }
+
+        System.out.println(answer);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        solution("ab6CDE443fgh22iJKlmn1o","6CD");
+        solution("ppprrrogrammers","pppp");
+        solution("AbcAbcA","AAA");
+
+    }
+}

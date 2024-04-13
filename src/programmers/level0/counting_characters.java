c5258a98607290a9cf923e6154ff822be0d43c43
leekun <dlrms6172@naver.com>
2024-04-14 00:44:47 +0900

    프로그래머스 > level 0.문자 개수 세기

---
+++ src/programmers/level0/counting_characters.java
@@ -0,0 +1,35 @@
+package programmers.level0;
+
+import java.util.Locale;
+
+/**
+ * TITLE : 문자 개수 세기
+ * LEVEL : 0
+ */
+public class counting_characters {
+
+    public static int[] solution(String my_string) {
+        int[] answer = new int[52];
+
+        for(int i = 0; i < my_string.length(); i++){
+
+            if(Character.isUpperCase(my_string.charAt(i))){
+                answer[(int)my_string.charAt(i) - 65]++;
+            }else{
+                answer[(int)my_string.charAt(i) - 65 - 6]++;
+            }
+        }
+
+
+        for(int i = 0; i < answer.length; i++){
+            System.out.print(answer[i]);
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        solution("Programmers");
+    }
+}

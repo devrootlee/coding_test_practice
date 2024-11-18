6b241d7bb78ec5fe17be8fa4b0e550e79a678701
이근 <dlrms617292@gmail.com>
2024-11-18 14:19:49 +0900

    프로그래머스 > codingLevel1.직사각형 별찍기

---
+++ src/programmers/coding/codingLevel1/rectangular_star.java
@@ -0,0 +1,23 @@
+package programmers.coding.codingLevel1;
+
+import java.util.Scanner;
+
+/**
+ * TITLE : 직사각형 별찍기
+ * LEVEL : 1
+ */
+public class rectangular_star {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for(int i = 0; i < b; i++){
+            for(int j = 0; j < a; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

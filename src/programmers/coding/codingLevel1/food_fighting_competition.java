851a42afa9bb38f6c4434b29626950c5935d959a
이근 <dlrms617292@gmail.com>
2024-11-20 10:09:20 +0900

    프로그래머스 > codingLevel1.푸드 파이트 대회

---
+++ src/programmers/coding/codingLevel1/food_fighting_competition.java
@@ -0,0 +1,36 @@
+package programmers.coding.codingLevel1;
+/**
+ * TITLE : 푸드 파이트 대회
+ * LEVEL : 1
+ */
+public class food_fighting_competition {
+
+    public static String solution(int[] food) {
+        String answer = "";
+        StringBuilder firstStep = new StringBuilder();
+        String water = "0";
+
+        for(int i = 1; i < food.length; i++){
+            int foodCnt = food[i] / 2;
+
+            for(int j = 0; j < foodCnt; j++){
+                firstStep.append(i);
+            }
+        }
+
+        StringBuilder red = new StringBuilder(firstStep);
+        StringBuilder blue = new StringBuilder(firstStep.reverse());
+        answer = red + water + blue;
+
+        System.out.println(answer);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1,3,4,6};
+        int[] arr2 = {1,7,1,2};
+
+        solution(arr1);
+        solution(arr2);
+    }
+}
